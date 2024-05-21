71e066c318561f10376a425d5c2ccda66a8eb864
Maxi Mize B. <136613072+MaxiMizeB@users.noreply.github.com>
2024-05-21 18:27:21 -0300

    Stamina Bar Added
    Stamina Bar Added

---
+++ MRC/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    [SerializeField] private Image staminaBarImage;
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AlterarStaminaBar(int staminaAtual, int staminaMaxima)
+    {
+        staminaBarImage.fillAmount = (float)staminaAtual / staminaMaxima;
+    }
+}
